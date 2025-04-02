d0898594a9c999aee9fd699701bd732ef9d15efa
Juan Carlos Lopez Puig <juancarlos91proyectos@hotmail.com>
2025-04-02 15:40:30 +0200

    cambio de appsettings y inyeccion de dependencias a proyecto principal en vez de proyecto WinUI

--- GestionVecinal/GestionVecinal.WinUI/MauiProgram.cs
+++ GestionVecinal/GestionVecinal.WinUI/MauiProgram.cs
@@ -15,73 +15,11 @@ namespace GestionVecinal.WinUI
     {
         public static MauiApp CreateMauiApp()
         {
-            var builder = MauiApp.CreateBuilder()
-                .RegisterServices()
-                .RegisterRepositories()
-                .RegisterViewsAndViewModels()
-                .RegisterAutoMapper()
-                .AddAppSettings();
-
-            
-
+            var builder = MauiApp.CreateBuilder();
             builder.UseSharedMauiApp();
 
             var app = builder.Build();
             return app;
-        }
-
-        private static MauiAppBuilder RegisterServices(this MauiAppBuilder builder)
-        {
-            builder.Services.AddTransient<IComunidadesService, ComunidadesService>();
-            builder.Services.AddTransient<ILoginService, LoginService>();
-            return builder;
-        }
-
-        private static MauiAppBuilder RegisterRepositories(this MauiAppBuilder builder)
-        {
-            builder.Services.AddTransient<IComunidadesRepository, ComunidadesRepository>();
-            return builder;
-        }
-
-        private static MauiAppBuilder RegisterViewsAndViewModels(this MauiAppBuilder builder)
-        {
-            
-            builder.Services.AddTransient<Login>();
-            builder.Services.AddTransient<CommunitySelect>();
-            builder.Services.AddTransient<AddComunidad>();
-            //builder.Services.AddTransient<MainPage>();
-
-
-            builder.Services.AddTransient<LoginViewModel>();
-            builder.Services.AddTransient<CommunitySelectViewModel>();
-            //builder.Services.AddTransient<MainPageViewModel>();
-            builder.Services.AddTransient<AddComunidadViewModel>();
-            return builder;
-        }
-
-        private static MauiAppBuilder RegisterAutoMapper(this MauiAppBuilder builder)
-        {
-            builder.Services.AddAutoMapper(typeof(ComunidadMappingProfile));
-            return builder;
-        }
-        private static MauiAppBuilder AddAppSettings(this MauiAppBuilder builder)
-        {
-            builder = builder.ConfigureAppSettings("appsettings.json");
-            return builder;
-        }
-        private static MauiAppBuilder ConfigureAppSettings(this MauiAppBuilder builder, string fileName)
-        {
-            var assembly = Assembly.GetExecutingAssembly();
-            
-            using Stream stream = assembly.GetManifestResourceStream($"GestionVecinal.WinUI.{fileName}")
-                    ?? throw new ArgumentNullException($"Stream can not be null     on {nameof(ConfigureAppSettings)}");
-
-            var config = new ConfigurationBuilder().AddJsonStream(stream).Build();
-            builder.Configuration.AddConfiguration(config);
-            
-            builder.Configuration.GetSection("AppSettings").Get<AppSettings>();
-            builder.Services.AddSingleton<AppSettings>(config.GetSection("AppSettings").Get<AppSettings>());
-            return builder;
         }
     }
 }
--- GestionVecinal/GestionVecinal/MauiProgramExtensions.cs
+++ GestionVecinal/GestionVecinal/MauiProgramExtensions.cs
@@ -1,5 +1,13 @@
-﻿using CommunityToolkit.Maui;
+﻿using GestionVecinal.Models.ViewModels;
+using GestionVecinal.Models;
+using GestionVecinal.Repositories;
+using GestionVecinal.Services.Interfaces;
+using GestionVecinal.Services.Mappers;
+using GestionVecinal.Services;
+using GestionVecinal.Views;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
+using System.Reflection;
 
 namespace GestionVecinal
 {
@@ -9,6 +17,11 @@ namespace GestionVecinal
         {
             builder
                 .UseMauiApp<App>()
+                .RegisterAutoMapper()
+                .RegisterServices()
+                .RegisterRepositories()
+                .RegisterViewsAndViewModels()
+                .AddAppSettings()
                 .ConfigureFonts(fonts =>
                 {
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
@@ -19,6 +32,60 @@ namespace GestionVecinal
     		builder.Logging.AddDebug();
 #endif
 
+            return builder;
+        }
+
+        private static MauiAppBuilder RegisterServices(this MauiAppBuilder builder)
+        {
+            builder.Services.AddTransient<IComunidadesService, ComunidadesService>();
+            builder.Services.AddTransient<ILoginService, LoginService>();
+            return builder;
+        }
+
+        private static MauiAppBuilder RegisterRepositories(this MauiAppBuilder builder)
+        {
+            builder.Services.AddTransient<IComunidadesRepository, ComunidadesRepository>();
+            return builder;
+        }
+
+        private static MauiAppBuilder RegisterViewsAndViewModels(this MauiAppBuilder builder)
+        {
+
+            builder.Services.AddTransient<Login>();
+            builder.Services.AddTransient<CommunitySelect>();
+            builder.Services.AddTransient<AddComunidad>();
+            //builder.Services.AddTransient<MainPage>();
+
+
+            builder.Services.AddTransient<LoginViewModel>();
+            builder.Services.AddTransient<CommunitySelectViewModel>();
+            //builder.Services.AddTransient<MainPageViewModel>();
+            builder.Services.AddTransient<AddComunidadViewModel>();
+            return builder;
+        }
+
+        private static MauiAppBuilder RegisterAutoMapper(this MauiAppBuilder builder)
+        {
+            builder.Services.AddAutoMapper(typeof(ComunidadMappingProfile));
+            return builder;
+        }
+        private static MauiAppBuilder AddAppSettings(this MauiAppBuilder builder)
+        {
+            builder = builder.ConfigureAppSettings("appsettings.json");
+            return builder;
+        }
+        private static MauiAppBuilder ConfigureAppSettings(this MauiAppBuilder builder, string fileName)
+        {
+            var assembly = Assembly.GetExecutingAssembly();
+
+            using Stream stream = assembly.GetManifestResourceStream($"GestionVecinal.{fileName}")
+                    ?? throw new ArgumentNullException($"Stream can not be null on {nameof(ConfigureAppSettings)}");
+
+            IConfigurationRoot config = new ConfigurationBuilder().AddJsonStream(stream).Build();
+            builder.Configuration.AddConfiguration(config);
+
+            builder.Configuration.GetSection("AppSettings").Get<AppSettings>();
+            builder.Services.AddSingleton<AppSettings>(config.GetSection("AppSettings").Get<AppSettings>());
             return builder;
         }
     }
